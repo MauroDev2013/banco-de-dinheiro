004f9bd9271e926a8209f021bf4ec76a4858ee1e
MauroDev2013 <mauro2013oliv@gmail.com>
2025-05-21 10:38:45 -0400

    banco criado com python ja funcional (completo)

--- banco_de_valores.py
+++ banco_de_valores.py
@@ -1,25 +1,63 @@
 cont_saque = 0
 LIMITE_SAQUE = 500
 extrato = 0
-
-
-tabela = print("""
-==========================
-|        BANCO           |
-==========================
-|   [s]Saque.            |
-|   [d]Deposito.         |
-|   [e]Extrato.          |
-|   [q]Sair.             |
-==========================
-    """)
-
-
-opção ========================== str(input("Digite uma opção:")).lower()[0]
+opção = ""
+lista_de_acontecimentos = []
+saque = 0
 
 while opção != "q":
-    if opção = "s":
-        saque = float(input("Valor do saque: "))
-        if saque > 500 or cont_saque == 3:
-            print("excedeu o valor do saque." if saque > 500 else "excedeu o numero de saques.")
 
+    tabela = print("""
+    ==========================
+            BANCO           
+    ==========================
+       [s]Saque.            
+       [d]Deposito.         
+       [e]Extrato.          
+       [q]Sair.             
+        """)
+    print("     ==========================")
+
+
+    opção = str(input("Digite uma opção:")).lower()[0]
+
+    if opção == "s":
+        saque = float(input("Valor do saque: "))
+        if saque > 500 or cont_saque == 3 or saque > extrato:
+            if saque > 500:
+                print("excedeu o valor do saque.")
+
+            elif saque > extrato:
+                print("excedeu o seu proprio dinheiro.")
+
+            else:
+                print("excedeu o numero de saques.")
+
+        elif saque < 0:
+            print("Não aceitamos numeros negativos.tente novamente.")
+
+        else:
+            cont_saque += 1
+            extrato -= saque
+            lista_de_acontecimentos.append(f"Saque: {saque:.2f}")
+
+    elif opção == "d":
+        deposito = float(input("Deposite o seu valor: "))
+
+        if saque < 0:
+            print("Não aceitamos numeros negativos.tente novamente.")
+
+        else:
+            extrato += deposito
+            lista_de_acontecimentos.append(f"Deposito: {deposito:.2f}")
+
+    elif opção == "e":
+        print("==========EXTRATO==========")
+
+        for item in lista_de_acontecimentos:
+            print(item)
+
+        print(f"Extrato: {extrato:.2f}")
+            
+
+        print("============================")
